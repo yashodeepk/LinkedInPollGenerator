264f8b80e9837b78c8e39ed2bba3167b57b2fb79
Yashodeep Kacholiya <yashodeepkacholiya@Yashodeeps-MacBook-Air.local>
2021-11-20 15:40:46 +0530

    basic template

--- linkedinpollgenerator/src/App.js
+++ linkedinpollgenerator/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import EmojiChecker from './components/emojiChecker';
+
+import heart from './assests/images/Linkedin-Heart.png';
+import like from './assests/images/LinkedIn-Like.png';
+import support from './assests/images/Linkedin-Support.png';
+import insight from './assests/images/Linkedin-Insightful.png';
+import curious from './assests/images/Linkedin-Curious.png';
+import celebrate from './assests/images/Linkedin-Celebrate.png';
 
 function App() {
   return (
     <div className="App">
-      <h1>LinkedIn Poll Generator</h1>
+      <h1>LinkedIn Reaction Poll Generator</h1>
+      <div>
+        <form>
+          <label>
+            <input type="text" value="Poll Title"/>
+          </label>
+        </form>
+        <p>POLL PERVIEW</p>
+      </div>
+      <div className="App-header">
+        <EmojiChecker type="like" img={like}/>
+        <EmojiChecker type="celebrate" img={celebrate}/>
+        <EmojiChecker type="support" img={support}/>
+        <EmojiChecker type="heart" img={heart}/>
+        <EmojiChecker type="insight" img={insight}/>
+        <EmojiChecker type="curious" img={curious}/>
+      </div>
+      <button type = "submit">
+        Submit and Download
+      </button>
     </div>
   );
 }
--- linkedinpollgenerator/src/components/emojiChecker.js
+++ linkedinpollgenerator/src/components/emojiChecker.js
@@ -1,9 +1,21 @@
+import './emojiChecker.css';
 
-export default emojiChecker()
+export default function EmojiChecker(props)
 {
+    const valueIs = "Enter label for ";
     return (
-        <div>
-            
+        <div className = "emoji-card" >
+            <input type="checkbox" id={props.type} name={props.type} value={props.type}/>
+            <img 
+                src={props.img}
+                style={{ width: 60, height: 70 , margin: 10}} 
+                alt={props.type}
+            />
+            <form>
+                <label>
+                    <input className="input-text-Style" type="text" value={valueIs} />
+                </label>
+            </form>
         </div>
     );
 }
