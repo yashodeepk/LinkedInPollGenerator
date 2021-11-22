f5b0ae5cc478ff7a21d96a6c00691f9ddda57122
Yashodeep Kacholiya <yashodeepkacholiya@Yashodeeps-MacBook-Air.local>
2021-11-22 22:48:43 +0530

    icon count added

--- linkedinpollgenerator/src/components/Canvas.js
+++ linkedinpollgenerator/src/components/Canvas.js
@@ -16,6 +16,13 @@ class ImageEditor extends Component
   {
     super(props);
     this.state = {
+      emojiCount: 2,
+      imgWidth: 150,
+      imgHeight: 150,
+      xPosition: 300,
+      yPosition: 350,
+      count: 1,
+      constantDistance: 300,
       title: '',
       imgData: "",      
       mainCanvasVisibleStyle: {
@@ -25,13 +32,26 @@ class ImageEditor extends Component
     }
     this.handleTitleChange = this.handleTitleChange.bind(this);
     this.addEmoji = this.addEmoji.bind(this);
+    this.handleEmojiChange = this.handleEmojiChange.bind(this);
+  }
+
+  handleEmojiChange(event) {
+    if(event.target.value>=1 && event.target.value <=6)
+    {
+      let canvas =  this.refs.resultCanvas;
+      const context = canvas.getContext("2d"); 
+
+      context.clearRect(0,0,canvas.width, canvas.height);
+
+      this.setState({emojiCount: event.target.value});
+    }
   }
 
   handleTitleChange(event) {    
     this.setState({title: event.target.value});
     this.showText(event.target.value);
   }
-  
+
   getCanvasMidPoint()
   {
     const canvas = this.refs.resultCanvas;
@@ -47,10 +67,10 @@ class ImageEditor extends Component
     let canvas =  this.refs.resultCanvas;
     const context = canvas.getContext("2d");
     let _this = this;
-
+    
     context.save();
     context.font = "bold 55px Arial";
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, 150);
     context.textAlign = 'center';
     context.fillText(title,canvas.width/2, 100);
     context.restore();
@@ -63,6 +83,15 @@ class ImageEditor extends Component
     let canvas =  this.refs.resultCanvas;
     const context = canvas.getContext("2d"); 
     let _this = this;
+
+    let imageWidth = this.state.imgWidth;
+    let imageHeight = this.state.imgHeight;
+    let constantDis = this.state.constantDistance;
+
+    let xPos = this.state.xPosition;
+    let yPos = this.state.yPosition;
+
+
     var image = new Image();
     if(type === "heart") image.src = heart;
     else if(type === "like") image.src = like;
@@ -70,63 +99,15 @@ class ImageEditor extends Component
     else if(type === "support") image.src = support;
     else if(type === "insight") image.src = insight;
     else if(type === "curious") image.src = curious;
-    image.onload = function() {  
-      context.clearRect(0, 0, canvas.width, canvas.height); 
-      let imgSize = {
-        width: image.width,
-        height: image.height
-      };
-      _this.setState({selection: imgSize}); 
+    image.onload = function() {
       context.save();
-      context.translate(_this.getCanvasMidPoint().x, _this.getCanvasMidPoint().y);      
-      context.drawImage(image, 0, 0);
+      context.textAlign = 'center';     
+      context.drawImage(image, xPos, yPos, imageWidth, imageHeight);
       context.restore();
       _this.setState({imgData: canvas.toDataURL('image/jpeg')});
       _this.setState({isSelectDisabled: false});
     };
   }
-
-  loadCanvas(imgData){
-    let canvas =  this.refs.resultCanvas;
-    const context = canvas.getContext("2d"); 
-    let _this = this;
-    
-    var image = new Image();
-    image.onload = function() {  
-      context.clearRect(0, 0, canvas.width, canvas.height); 
-      let imgSize = {
-        width: image.width,
-        height: image.height
-      };
-      _this.setState({selection: imgSize}); 
-      context.save();
-      context.translate(_this.getCanvasMidPoint().x, _this.getCanvasMidPoint().y);      
-      context.drawImage(image, 0, 0);
-      context.restore();
-      _this.setState({imgData: canvas.toDataURL('image/jpeg')});
-      _this.setState({isSelectDisabled: false});
-    };
-    image.src =  this.state.imgData;
-  }
-
-  
-  
-  loadFile()
-  {
-     var file = document.querySelector('input[type=file]').files[0];
-     var reader = new FileReader();
-     var _this = this;
-
-     reader.onloadend = function () {
-       _this.setState({imgData: reader.result});
-       _this.loadCanvas(reader.result);           
-     }
-     
-     if (file) {
-       reader.readAsDataURL(file); 
-     }   
-  }
-
 
   downloadImg()
   {
@@ -151,6 +132,12 @@ class ImageEditor extends Component
       this.showImage(type);
       console.log("Emoji added");
     }
+    else
+    {
+      let canvas =  this.refs.resultCanvas;
+      const context = canvas.getContext("2d"); 
+      context.clearRect(0,0,canvas.width, canvas.height);
+    }
   }
 
   render(){
@@ -165,6 +152,16 @@ class ImageEditor extends Component
             value={this.state.title} 
             onChange={this.handleTitleChange}
             style={{fontSize: 20, width: 350}}
+          />
+          <label style={{fontSize: 20, marginLeft: 10}}>
+            Icon Count
+          </label>
+          <input 
+            type="number" 
+            placeholder="EmojiCount" 
+            value={this.state.emojiCount} 
+            onChange={this.handleEmojiChange}
+            style={{fontSize: 20, width: 50, marginLeft: 4}}
           />
         </div>       
         <canvas className="canvas" ref="resultCanvas" style={this.state.mainCanvasVisibleStyle} width="1200" height="628"/>
